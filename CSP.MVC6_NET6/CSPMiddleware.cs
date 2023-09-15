5b9b59153acd17a22de50304611e40ea2c2ba0fa
Che Smith <che.smith.usa@gmail.com>
2023-09-15 09:18:12 -0500

    Add project files.

---
+++ CSP.MVC6_NET6/CSPMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CSP.MVC_NET6
+{
+    public class CSPMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CSPMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string nonce = GenerateNonce();
+            string policy = $"default-src 'self' 'nonce-{nonce}'; script-src-attr 'unsafe-inline'; connect-src  wss://localhost:*";
+
+            context.Response.Headers.Add("Content-Security-Policy", policy);
+
+            // You can also add other security headers if needed, like X-Content-Type-Options, X-Frame-Options, etc.
+
+            // Intercept the response body stream
+            Stream originalBody = context.Response.Body;
+            using (var newBody = new MemoryStream())
+            {
+                context.Response.Body = newBody;
+
+                await _next(context);
+
+                // Rewind the stream for reading
+                newBody.Seek(0, SeekOrigin.Begin);
+                string responseBody = new StreamReader(newBody).ReadToEnd();
+
+                // Replace '{nonce}' with the actual nonce
+                responseBody = responseBody.Replace("{nonce}", nonce);
+
+                // Write the modified content back to the response
+                await originalBody.WriteAsync(System.Text.Encoding.UTF8.GetBytes(responseBody), 0, responseBody.Length);
+            }
+        }
+
+        private string GenerateNonce()
+        {
+            var nonceBytes = new byte[32];
+            var generator = RandomNumberGenerator.Create();
+            generator.GetBytes(nonceBytes);
+            return Convert.ToBase64String(nonceBytes);
+        }
+    }
+}
---
+++ CSP.MVC6_NET6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSP.MVC6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
